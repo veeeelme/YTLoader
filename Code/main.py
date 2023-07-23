1510db67df9a79ac3e375633bf81b2ad5e0e17d4
VeelMe <138215410+veeeelme@users.noreply.github.com>
2023-07-23 11:39:38 +0300

    Update main.py
    Update! Now you can download files with your own resolution.

--- Code/main.py
+++ Code/main.py
@@ -1,15 +1,16 @@
 from pytube import YouTube
 
-path = 'YOUR PATH HERE'
+path = 'YOUR PATH THERE'
 
 
 link = input('Enter URL of video >> ')
+resolution = input('Enter resolution(144p, 240p, 360p, 720p, 1080p...) >> ')
 
 
 def download():
     try:
         yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
-        stream = yt.streams.get_lowest_resolution()
+        stream = yt.streams.get_by_resolution(resolution)
         stream.download(path)
     except Exception as e:
         print(f'oops! Error: {e}')
@@ -17,3 +18,4 @@ def download():
 
 print('wait a minute...')
 download()
+print('Finish! Your file is in ', path)
